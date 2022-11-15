cf81e64490389fc51dadf32f31292ce3ac5dc095
joancoll <joancoll@inspladelestany.cat>
2022-11-15 14:10:54 +0100

    Get color from Theme

--- app/src/main/java/cat/dam/andy/menulayout/MainActivity.java
+++ app/src/main/java/cat/dam/andy/menulayout/MainActivity.java
@@ -1,9 +1,8 @@
 package cat.dam.andy.menulayout;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-
+import android.content.Context;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
@@ -21,34 +20,49 @@ public class MainActivity extends AppCompatActivity {
         //Per defecte mostra el primer layout
         View child = getLayoutInflater().inflate(R.layout.layout, null);
         fl_content.addView(child);
-        btn_lyt1.setBackgroundColor(ContextCompat.getColor(this, com.google.android.material.R.color.design_default_color_primary_variant));
+        TypedValue typedValue = new TypedValue();
+        btn_lyt1.setBackgroundColor(getThemePrimaryVariantColor(this));
         //en cas de que es premi algun dels botons mostrarà el layout corresponent
         btn_lyt1.setOnClickListener(v -> {
             System.out.println("Button 1 Clicked");
-            btn_lyt1.setBackgroundColor(ContextCompat.getColor(this, com.google.android.material.R.color.design_default_color_primary_variant));
-            btn_lyt2.setBackgroundColor(ContextCompat.getColor(this, com.google.android.material.R.color.design_default_color_primary));
-            btn_lyt3.setBackgroundColor(ContextCompat.getColor(this, com.google.android.material.R.color.design_default_color_primary));
+            btn_lyt1.setBackgroundColor(getThemePrimaryVariantColor(this));
+            btn_lyt2.setBackgroundColor(getThemePrimaryColor(this));
+            btn_lyt3.setBackgroundColor(getThemePrimaryColor(this));
             fl_content.removeAllViews();
             View child1 = getLayoutInflater().inflate(R.layout.layout, null);
             fl_content.addView(child1);
         });
         btn_lyt2.setOnClickListener(v -> {
             System.out.println("Button 2 Clicked");
-            btn_lyt1.setBackgroundColor(ContextCompat.getColor(this, com.google.android.material.R.color.design_default_color_primary));
-            btn_lyt2.setBackgroundColor(ContextCompat.getColor(this, com.google.android.material.R.color.design_default_color_primary_variant));
-            btn_lyt3.setBackgroundColor(ContextCompat.getColor(this, com.google.android.material.R.color.design_default_color_primary));
+            btn_lyt1.setBackgroundColor(getThemePrimaryColor(this));
+            btn_lyt2.setBackgroundColor(getThemePrimaryVariantColor(this));
+            btn_lyt3.setBackgroundColor(getThemePrimaryColor(this));
             fl_content.removeAllViews();
             View child2 = getLayoutInflater().inflate(R.layout.layout2, null);
             fl_content.addView(child2);
         });
         btn_lyt3.setOnClickListener(v -> {
             System.out.println("Button 3 Clicked");
-            btn_lyt1.setBackgroundColor(ContextCompat.getColor(this, com.google.android.material.R.color.design_default_color_primary));
-            btn_lyt2.setBackgroundColor(ContextCompat.getColor(this, com.google.android.material.R.color.design_default_color_primary));
-            btn_lyt3.setBackgroundColor(ContextCompat.getColor(this, com.google.android.material.R.color.design_default_color_primary_variant));
+            btn_lyt1.setBackgroundColor(getThemePrimaryColor(this));
+            btn_lyt2.setBackgroundColor(getThemePrimaryColor(this));
+            btn_lyt3.setBackgroundColor(getThemePrimaryVariantColor(this));
             fl_content.removeAllViews();
             View child3 = getLayoutInflater().inflate(R.layout.layout3, null);
             fl_content.addView(child3);
         });
+    }
+    private static int getThemePrimaryColor(Context context) {
+        int colorAttr;
+        colorAttr = context.getResources().getIdentifier("colorPrimary", "attr", context.getPackageName());
+        TypedValue outValue = new TypedValue();
+        context.getTheme().resolveAttribute(colorAttr, outValue, true);
+        return outValue.data;
+    }
+    private static int getThemePrimaryVariantColor(Context context) {
+        int colorAttr;
+        colorAttr = context.getResources().getIdentifier("colorPrimaryVariant", "attr", context.getPackageName());
+        TypedValue outValue = new TypedValue();
+        context.getTheme().resolveAttribute(colorAttr, outValue, true);
+        return outValue.data;
     }
 }
